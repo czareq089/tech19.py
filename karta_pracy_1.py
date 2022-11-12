c42a1479cf52342a808ca122c9b8b33214a83074
czareq089 <86021893+czareq089@users.noreply.github.com>
2022-11-12 20:47:54 +0100

    Add files via upload
    nie chciało mi się ale to zrobiłem

---
+++ karta_pracy_1.py
@@ -0,0 +1,52 @@
+#1
+
+a = int(input())
+b = int(input())
+
+suma = a * a + b * b 
+
+print(suma)
+
+#2 
+
+a = int(input())
+b = int(input())
+
+suma = (a+b)**2
+
+print(suma)
+
+#3
+
+a = int(input())
+b = int(input())
+
+suma = (a-b)**3
+
+print(suma)
+
+#4
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a*b*c)
+
+#5
+
+a = int(input())
+b = int(input())
+
+print(float(2*(a+b)/5))
+print(float((a+b)*0.4))
+
+#6
+brutto = int(input())
+print(brutto/1.23)
+
+#7
+a = int(input())
+b = int(input())
+
+print(a%b)
